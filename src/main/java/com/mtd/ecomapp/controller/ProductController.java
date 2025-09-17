a6ca6672973cd2ff53ebedb89ccde72ce33d2287
Deepika-cp <deepikamoganna@gmail.com>
2025-09-17 11:23:49 +0530

    new

--- src/main/java/com/mtd/ecomapp/controller/ProductController.java
+++ src/main/java/com/mtd/ecomapp/controller/ProductController.java
@@ -1,8 +1,7 @@
 package com.mtd.ecomapp.controller;
-
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,38 +10,35 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.mtd.EcomApp.entity.Product;
-import com.mtd.EcomApp.service.ProductService;
+
+import com.mtd.ecomapp.entity.Product;
+import com.mtd.ecomapp.service.Productservice;
 
 @RestController
-@RequestMapping("/products")
-@CrossOrigin("*")
+@RequestMapping("/Products")
 public class ProductController {
 	@Autowired
-	private ProductService productService;
-	
+	private Productservice productService;
 	@PostMapping("/save")
 	public Product save(@RequestBody Product product) {
-		return productService.saveProduct(product); 
+		return productService.saveProduct(product);
+		
 	}
-	
 	@GetMapping("/{id}")
 	public Product findById(@PathVariable String id) {
 		return productService.getProductById(id);
 	}
-
 	@GetMapping("/all")
 	public List<Product> findAll(){
 		return productService.getProducts();
 	}
-
 	@PutMapping("/{id}")
-	public Product updateProduct(@RequestBody Product product, @PathVariable String id) {
+	public Product updateproduct(@RequestBody Product product,@PathVariable String id) {
 		return productService.updateProduct(product, id);
 	}
-
 	@DeleteMapping("/{id}")
 	public boolean deleteProduct(@PathVariable String id) {
-		return productService.deleteProduct(id);
+		return productService.deleteProducts(id);
 	}
+
 }
